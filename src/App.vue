3df9a62886d7d0e4ab8a2b06580ead5ca6e3a7f2
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-12-04 10:45:59 -0500

    init mockup build

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+    <div class="card card-body mb-3">
+      <em>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore.
+        <a href="#">Am I a Hillsborough County customer?</a>
+      </em>
+
+      <div class="form-check mt-2">
+        <input v-model="varify" class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+          I am a customer of Hillsborough County
+        </label>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <a class="btn btn-primary btn-lg btn-block text-white" :class="btnVarified">
+          Get in Line
+          <span class="fas fa-fw fa-ticket-alt"></span>
+        </a>
+      </div>
+      <div class="col-md-6">
+        <a href="#" class="btn btn-primary btn-lg btn-block">
+          My Tickets
+          <span class="fas fa-fw fa-user-circle"></span>
+        </a>
+      </div>
+    </div>
+
+    <table is="IwtTable" class="my-3"></table>
+
+    <h3>Lorem Ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
   </div>
 </template>
 
 <script>
+import IwtTable from './components/Iwt'
 export default {
-  name: 'app',
+  components: { IwtTable },
   data () {
     return {
+      varify: false,
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    btnVarified () {
+      return (this.varify) ? '' : 'disabled'
     }
   }
 }
---
+++ src/components/Iwt.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <table class="table table-sm table-striped table-bordered">
+    <thead class="bg-secondary text-white">
+      <tr>
+        <th>Service</th>
+        <th>Location</th>
+        <th>Customers Waiting</th>
+        <th>Apprx. Wait Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="n in 10">
+        <td>Apply for a Building Permit</td>
+        <td>County Center</td>
+        <td>{{ n }}</td>
+        <td>{{ n }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+}
+</script>
